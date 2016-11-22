31df3e00ed2b8d1b1b83aa6c9a09061e9dbb89e6
Vikas Maggo <Vikas Maggo>
2016-11-22 16:21:16 +0530

    Document Reverse

--- R/reverse.R
+++ R/reverse.R
@@ -1,5 +1,7 @@
 #' Reverse a vector
 #'
+#'Reverse returns the copy of a vector whose elements are in the reverse order. The order.
+#'
 #' @param x
 #'
 #' @return  a vector
@@ -9,7 +11,6 @@
 #' x <- 1:10
 #' reverse(x)
 reverse <- function(x) {
-if(length(x) > 0 ) {
-    x[length(x):1]  else x̥
-    }
+if(length(x) >  0 ) x[length(x):1]  else x
 }
+
