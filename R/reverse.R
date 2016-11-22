a56866fdcd93fb5e5edd1045ba424a9563ff9013
Vikas Maggo <Vikas Maggo>
2016-11-22 16:14:04 +0530

    Added Reverse function

---
+++ R/reverse.R
@@ -0,0 +1,15 @@
+#' Reverse a vector
+#'
+#' @param x
+#'
+#' @return  a vector
+#' @export
+#'
+#' @examples
+#' x <- 1:10
+#' reverse(x)
+reverse <- function(x) {
+if(length(x) > 0 ) {
+    x[length(x):1]  else x̥
+    }
+}
